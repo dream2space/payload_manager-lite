dfe5493d6d7bd64e9c4ae843114f9261b1f2b2f4
huiminlim <e0036379@u.nus.edu>
2021-04-28 22:49:17 +0800

    add completed camera test

--- 01-camera_test.py
+++ 01-camera_test.py
@@ -1,5 +1,17 @@
 from picamera import PiCamera
+from time import sleep
 
 # Initialize Camera object
 camera = PiCamera()
 camera.resolution = (640, 480)
+sleep(1)  # Warmup
+
+# Show preview of camera for 5 seconds
+camera.start_preview()
+sleep(5)
+camera.stop_preview()
+
+camera.capture('/home/pi/Desktop/image.jpg')
+print(f"Captured image! Stored here -> {'/home/pi/Desktop/image.jpg'}")
+
+camera.close()
