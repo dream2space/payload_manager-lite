17ff7308625c2b6e95605a8a3b3ee2c99087bab4
huiminlim <e0036379@u.nus.edu>
2021-06-07 18:35:49 +0800

    fix newline

--- payload_manager.py
+++ payload_manager.py
@@ -53,7 +53,7 @@ def main(use_camera, use_downlink):
         while True:
             print("Waiting for commands...")
             try:
-                read_command = ser_cmd_input.readline().decode("utf-8").replace("\r\n", "")
+                read_command = ser_cmd_input.readline().decode("utf-8").replace("\n", "")
             except UnicodeDecodeError:
                 print("\nUnicode Decode Error\n")
                 time.sleep(1)
